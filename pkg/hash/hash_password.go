568497c9c5604f5a5695c2eab3e3102ce8ec239a
dev-ayaa <ayaaakinleye@gmail.com>
2022-11-17 22:09:57 +0100

    added function to hash password

--- pkg/hash/hash_password.go
+++ pkg/hash/hash_password.go
@@ -1,5 +1,34 @@
-package main
+package hash
 
-func main() {
+import (
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+)
 
+func HashPassword(password string) (string, error) {
+	if password == "" {
+		return "", fmt.Errorf("no input value")
+	} else {
+		fromPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+		if err != nil {
+			return fmt.Sprint("cannot generate encrypted password"), err
+		}
+		hashedString := string(fromPassword)
+		return hashedString, nil
+	}
+
+}
+
+func VerifyPassword(password, hashedPassword string) (bool, error) {
+	if password == "" || hashedPassword == "" {
+		return false, nil
+	}
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	if err != nil {
+		if err == bcrypt.ErrMismatchedHashAndPassword {
+			return false, fmt.Errorf("invalid string comparision : %v", err)
+		}
+		return false, err
+	}
+	return true, nil
 }
