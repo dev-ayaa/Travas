9cc8f6d4f721466a525ffaf1ebe905267891658a
AbdulkadirAbdullahi <58042388+AbdulkadirAbdullahi@users.noreply.github.com>
2022-11-17 17:43:03 -0800

    Update model.go

--- model/model.go
+++ model/model.go
@@ -7,23 +7,18 @@ import (
 )
 
 type Tourist struct {
-	ID        primitive.ObjectID `json:"_id" bson:"_id"`
-	Name      string             `json:"name" Usage:"required,alpha" json:"name,omitempty"`
-	Email     string             `json:"email" Usage:"required,alphanumeric"`
-	Password  string             `json:"password" Usage:"required"`
-	Address   string             `json:"address"`
-	Tours     []ReservedTour     `json:"tours"`
-	Token     string             `json:"token" Usage:"jwt"`
-	NewToken  string             `json:"new_token" Usage:"jwt"`
-	CreatedAt time.Time          `json:"created_at" Usage:"datetime"`
-	UpdatedAt time.Time          `json:"updated_at" Usage:"datetime"`
+	ID            primitive.ObjectID `json:"_id" bson:"_id"`
+	Name          string             `json:"name" Usage:"required,alpha" json:"name,omitempty"`
+	Email         string             `json:"email" Usage:"required,alphanumeric"`
+	Password      string             `json:"password" Usage:"required"`
+	Address       string             `json:"address"`
+	ReservedTours []Tour             `json:"tours_list"`
+	Token         string             `json:"token" Usage:"jwt"`
+	NewToken      string             `json:"new_token" Usage:"jwt"`
+	CreatedAt     time.Time          `json:"created_at" Usage:"datetime"`
+	UpdatedAt     time.Time          `json:"updated_at" Usage:"datetime"`
 }
 
-type ReservedTour struct {
-	//Selected tour will be added here
-	ID       primitive.ObjectID `json:"_id"`
-	TourList []Tour             `json:"tour_list"`
-}
 type Tour struct {
 	ID              primitive.ObjectID `json:"_id"`
 	TourTitle       string             `json:"tour_title"`
