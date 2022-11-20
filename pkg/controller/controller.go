088fc8310513ff0fe01b16546c01fec2dba93ef3
dev-ayaa <ayaaakinleye@gmail.com>
2022-11-20 16:00:57 +0100

    create new and existing user api

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -1,23 +1,27 @@
 package controller
 
 import (
-	"fmt"
+	"errors"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/travas-io/travas/model"
 	"github.com/travas-io/travas/pkg/config"
+	"github.com/travas-io/travas/pkg/hash"
 	"github.com/travas-io/travas/query"
 	"github.com/travas-io/travas/query/repo"
-	"net/http"
-
 	"go.mongodb.org/mongo-driver/mongo"
+	"log"
+	"net/http"
+	"reflect"
 )
 
-type TravasHandler struct {
-	App *config.TravasConfig
+type Travas struct {
+	App *config.Tools
 	DB  query.TravasDBRepo
 }
 
-func NewTravasHandler(app *config.TravasConfig, db *mongo.Client) *TravasHandler {
-	return &TravasHandler{
+func NewTravas(app *config.Tools, db *mongo.Client) *Travas {
+	return &Travas{
 		App: app,
 		DB:  repo.NewTravasDB(app, db),
 	}
@@ -25,11 +29,74 @@ func NewTravasHandler(app *config.TravasConfig, db *mongo.Client) *TravasHandler
 
 // todo -> this is where all our handler/ controller logic will be done
 
-func (tv *TravasHandler) HomePage() gin.HandlerFunc {
+func (tr *Travas) Home() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		fmt.Println("Travas HomePage")
+		//Todo : render the home page of the application
+		ctx.JSON(http.StatusOK, gin.H{})
+	}
+}
+
+func (tr *Travas) Register() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{})
+	}
+}
+func (tr *Travas) ProcessRegister() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var user model.Tourist
+
 		if err := ctx.Request.ParseForm(); err != nil {
-			_ = ctx.AbortWithError(http.StatusInternalServerError, gin.Error{Err: err})
+			_ = ctx.AbortWithError(http.StatusBadRequest, gin.Error{Err: err})
+		}
+		user.Email = ctx.Request.Form.Get("email")
+		user.FirstName = ctx.Request.Form.Get("first_name")
+		user.LastName = ctx.Request.Form.Get("last_name")
+		user.Phone = ctx.Request.Form.Get("phone")
+		user.Password, _ = hash.Encrypt(ctx.Request.Form.Get("password"))
+		user.CheckPassword, _ = hash.Encrypt(ctx.Request.Form.Get("check_password"))
+
+		ok := reflect.DeepEqual(user.Password, user.CheckPassword)
+		if !ok {
+			_ = ctx.AbortWithError(http.StatusInternalServerError, errors.New("passwords did not match"))
+		}
+
+		if err := tr.App.Validator.Struct(&user); err != nil {
+			if _, ok := err.(*validator.InvalidValidationError); !ok {
+				_ = ctx.AbortWithError(http.StatusBadRequest, gin.Error{Err: err})
+				log.Println(err)
+				return
+			}
+		}
+		track, userID, err := tr.DB.InsertUser(user)
+		if err != nil {
+			_ = ctx.AbortWithError(http.StatusBadRequest, errors.New("error while adding new user"))
+			return
+		}
+
+		ctx.Header("Content-Type", "application/json")
+
+		data := model.IntraData{
+			ID:       userID,
+			Email:    user.Email,
+			Password: user.Password,
+		}
+		tr.App.Session.Put(ctx.Request.Context(), "data", data)
+
+		switch track {
+		case 1:
+			//add the user id to session
+			//redirect to the home page of the application
+			ctx.JSON(http.StatusSeeOther, gin.H{
+				"message": "Existing Account, Go to the Login page",
+			})
+		case 0:
+			//	after inserting new user to the database
+			//  notify the user to verify their  details via mail
+			//  OR
+			//  Send notification message on the page for them to login
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "Registered Successfully",
+			})
 		}
 
 	}
--- query/repo/db_query.go
+++ query/repo/db_query.go
@@ -1,7 +1,53 @@
 package repo
 
+import (
+	"context"
+	"github.com/travas-io/travas/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"time"
+)
+
 // database queries is done in this file
 
-func (td TravasDB) InsertUser() {
-	return
+func (td *TravasDB) InsertUser(user model.Tourist) (int, primitive.ObjectID, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+
+	var id primitive.ObjectID
+
+	filter := bson.D{{"email", user.Email}}
+	var res bson.M
+	err := TouristData(td.DB, "tourist").FindOne(ctx, filter).Decode(&res)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			user.ID = primitive.NewObjectID()
+			doc := bson.D{
+				{"email", user.Email},
+				{"first_name", user.FirstName},
+				{"last_name", user.LastName},
+				{"phone", user.Phone},
+				{"password", user.Password},
+			}
+			_, insertErr := TouristData(td.DB, "tourist").InsertOne(ctx, doc)
+			if insertErr != nil {
+				td.App.ErrorLogger.Fatalf("cannot add user to the database : %v ", insertErr)
+			}
+			return 0, user.ID, nil
+		}
+		td.App.ErrorLogger.Fatal(err)
+	}
+
+	for k, v := range res {
+		if k == "_id" {
+			switch userID := v.(type) {
+			case primitive.ObjectID:
+				id = userID
+			}
+		}
+
+	}
+	return 1, id, err
+
 }
