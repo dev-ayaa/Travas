e29424537469128b8f7a12229edfde5f8291ff3c
dev-ayaa <ayaaakinleye@gmail.com>
2022-11-21 13:37:19 +0100

    made changes to the db name and db uri

--- query/repo/db_collection.go
+++ query/repo/db_collection.go
@@ -5,6 +5,6 @@ import "go.mongodb.org/mongo-driver/mongo"
 // todo -> this file is where the application will be connected to collection in database
 
 func TouristData(db *mongo.Client, collection string) *mongo.Collection {
-	var touristData = db.Database("travas").Collection("tourist")
+	var touristData = db.Database("travasdb").Collection(collection)
 	return touristData
 }
