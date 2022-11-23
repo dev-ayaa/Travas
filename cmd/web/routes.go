bef4abbe8c5ceef384ba0476cd23315d10a7fb99
dev-ayaa <ayaaakinleye@gmail.com>
2022-11-24 00:27:51 +0100

    cors added for different origin

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,13 +4,16 @@ import (
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 	"github.com/travas-io/travas/pkg/controller"
 )
 
 func Routes(r *gin.Engine, t controller.Travas) {
 	router := r.Use(gin.Logger(), gin.Recovery())
+	router.Use(cors.Default())
 
 	cookieData := cookie.NewStore([]byte("travas"))
+	
 	router.Use(sessions.Sessions("session", cookieData))
 	router.GET("/", t.Welcome())
 	router.GET("/api/register", t.Register())
