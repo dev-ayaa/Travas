314cc801a3015742adb48a8f83affc67a2bd5fc7
dev-ayaa <ayaaakinleye@gmail.com>
2022-11-23 23:14:37 +0100

    implement gin contrib session

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -3,6 +3,11 @@ package controller
 import (
 	"errors"
 	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"github.com/travas-io/travas/model"
@@ -12,9 +17,6 @@ import (
 	"github.com/travas-io/travas/query"
 	"github.com/travas-io/travas/query/repo"
 	"go.mongodb.org/mongo-driver/mongo"
-	"log"
-	"net/http"
-	"reflect"
 )
 
 type Travas struct {
@@ -33,7 +35,7 @@ func NewTravas(app *config.Tools, db *mongo.Client) *Travas {
 
 func (tr *Travas) Welcome() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		//Todo : render the home page of the application
+		// Todo : render the home page of the application
 		ctx.JSON(http.StatusOK, gin.H{})
 	}
 }
@@ -43,6 +45,7 @@ func (tr *Travas) Register() gin.HandlerFunc {
 		ctx.JSON(http.StatusOK, gin.H{})
 	}
 }
+
 func (tr *Travas) ProcessRegister() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		var user model.Tourist
@@ -54,13 +57,17 @@ func (tr *Travas) ProcessRegister() gin.HandlerFunc {
 		user.FirstName = ctx.Request.Form.Get("first_name")
 		user.LastName = ctx.Request.Form.Get("last_name")
 		user.Phone = ctx.Request.Form.Get("phone")
-		user.Password, _ = hash.Encrypt(ctx.Request.Form.Get("password"))
-		user.CheckPassword, _ = hash.Encrypt(ctx.Request.Form.Get("check_password"))
+		user.Password = ctx.Request.Form.Get("password")
+		user.CheckPassword = ctx.Request.Form.Get("check_password")
+		user.CreatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
 
-		ok := reflect.DeepEqual(user.Password, user.CheckPassword)
-		if !ok {
+		if user.Password != user.CheckPassword {
 			_ = ctx.AbortWithError(http.StatusInternalServerError, errors.New("passwords did not match"))
 		}
+
+		user.Password, _ = hash.Encrypt(user.Password)
+		user.CheckPassword, _ = hash.Encrypt(user.CheckPassword)
 
 		if err := tr.App.Validator.Struct(&user); err != nil {
 			if _, ok := err.(*validator.InvalidValidationError); !ok {
@@ -69,26 +76,31 @@ func (tr *Travas) ProcessRegister() gin.HandlerFunc {
 				return
 			}
 		}
-		track, userID, err := tr.DB.InsertUser(user)
+		tours := []model.Tour{}
+		track, userID, err := tr.DB.InsertUser(user, tours)
 		if err != nil {
 			_ = ctx.AbortWithError(http.StatusBadRequest, errors.New("error while adding new user"))
 			return
 		}
-
-		ctx.Header("Content-Type", "application/json")
+		cookieData := sessions.Default(ctx)
 
 		data := model.IntraData{
 			ID:       userID,
 			Email:    user.Email,
 			Password: user.Password,
 		}
-		tr.App.Session.Put(ctx.Request.Context(), "data", data)
+		cookieData.Set("data", data)
 
+		if err := cookieData.Save(); err != nil {
+			log.Println("error from the session storage")
+			_ = ctx.AbortWithError(http.StatusNotFound, gin.Error{Err: err})
+			return
+		}
 		switch track {
 		case 1:
-			//add the user id to session
-			//redirect to the home page of the application
-			ctx.JSON(http.StatusSeeOther, gin.H{
+			// add the user id to session
+			// redirect to the home page of the application
+			ctx.JSON(http.StatusOK, gin.H{
 				"message": "Existing Account, Go to the Login page",
 			})
 		case 0:
@@ -100,7 +112,6 @@ func (tr *Travas) ProcessRegister() gin.HandlerFunc {
 				"message": "Registered Successfully",
 			})
 		}
-
 	}
 }
 
@@ -109,6 +120,7 @@ func (tr *Travas) LoginPage() gin.HandlerFunc {
 		ctx.JSON(http.StatusOK, gin.H{})
 	}
 }
+
 func (tr *Travas) ProcessLogin() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		if err := ctx.Request.ParseForm(); err != nil {
@@ -117,7 +129,9 @@ func (tr *Travas) ProcessLogin() gin.HandlerFunc {
 		email := ctx.Request.Form.Get("email")
 		password := ctx.Request.Form.Get("password")
 
-		data := tr.App.Session.Get(ctx.Request.Context(), "data").(model.IntraData)
+		cookieData := sessions.Default(ctx)
+		data := cookieData.Get("data").(model.IntraData)
+
 		verified, err := hash.Verify(password, data.Password)
 		if err != nil {
 			_ = ctx.AbortWithError(http.StatusInternalServerError, errors.New("cannot verify user input password"))
@@ -130,34 +144,32 @@ func (tr *Travas) ProcessLogin() gin.HandlerFunc {
 				if checkErr != nil {
 					_ = ctx.AbortWithError(http.StatusNotFound, fmt.Errorf("unregistered user %v", checkErr))
 				}
-				//generate the jwt token
+				// generate the jwt token
 				t1, t2, err := token.Generate(data.Email, data.ID)
 				if err != nil {
 					_ = ctx.AbortWithError(http.StatusInternalServerError, fmt.Errorf("token no generated : %v ", err))
-
 				}
 
 				var tk map[string]string
 				tk = map[string]string{"t1": t1, "t2": t2}
 
-				//update the database adding the token to user database
+				// update the database adding the token to user database
 				_, updateErr := tr.DB.UpdateInfo(data.ID, tk)
 				if updateErr != nil {
 					_ = ctx.AbortWithError(http.StatusNotFound, fmt.Errorf("unregistered user %v", updateErr))
-
 				}
-				ctx.Header("Content-Type", "application/json")
-				ctx.Header("Authorization", fmt.Sprintf("Bearer,%v", t1))
 
-				ctx.Redirect(http.StatusSeeOther, "/user/home")
+				ctx.SetCookie("authorization", t1, 60*60*24*7, "/", "localhost", false, true)
+				ctx.JSON(http.StatusOK, gin.H{"message": "Welcome to user homepage"})
+
+				// ctx.Redirect(http.StatusSeeOther, "api/auth/user/home")
 			}
 		}
-
 	}
 }
 
 func (tr *Travas) Main() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-
+		ctx.JSON(http.StatusOK, gin.H{"message": "Welcome to user homepage"})
 	}
 }
