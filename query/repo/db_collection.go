bea0324d9815a76a4bcd05e5deeb4a87f1a29d84
dev-ayaa <ayaaakinleye@gmail.com>
2022-11-28 15:39:03 +0100

    new collector for tours

--- query/repo/db_collection.go
+++ query/repo/db_collection.go
@@ -4,12 +4,17 @@ import "go.mongodb.org/mongo-driver/mongo"
 
 // todo -> this file is where the application will be connected to collection in database
 
-func TouristData(db *mongo.Client, collection string) *mongo.Collection {
+func TouristsData(db *mongo.Client, collection string) *mongo.Collection {
 	var touristData = db.Database("travasdb").Collection(collection)
 	return touristData
 }
 
-func OperatorData(db *mongo.Client, collection string) *mongo.Collection {
-	var touristData = db.Database("travasdb").Collection(collection)
-	return touristData
+func OperatorsData(db *mongo.Client, collection string) *mongo.Collection {
+	var operatorData = db.Database("travasdb").Collection(collection)
+	return operatorData
+}
+
+func ToursData(db *mongo.Client, collection string) *mongo.Collection {
+	var tourData = db.Database("travasdb").Collection(collection)
+	return tourData
 }
