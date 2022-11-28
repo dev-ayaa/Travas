f01de8608d7b9bd4a93f6796987fa06667e7b125
dev-ayaa <ayaaakinleye@gmail.com>
2022-11-28 03:12:06 +0100

    removed unsed db queries

--- query/db_repo.go
+++ query/db_repo.go
@@ -8,12 +8,15 @@ import (
 // todo -> all our queries method are to implement the interface
 
 type TravasDBRepo interface {
-	InsertUser(user model.Tourist, tours []model.Tour) (int, primitive.ObjectID, error)
+	InsertUser(user model.Tourist) (int, primitive.ObjectID, error)
 	CheckForUser(userID primitive.ObjectID) (bool, error)
 	UpdateInfo(userID primitive.ObjectID, tk map[string]string) (bool, error)
-	InsertTour(tour model.Tour) (primitive.ObjectID, error)
-	DeleteTour(tourID string) (bool, error)
-	UpdateTour(tourID string, tour model.Tour) (bool, error)
-	GetTour(tourID string) (tour model.Tour, err error)
-	FindAllTours() (tours []model.Tour, err error)
+	LoadPackage(res []model.Operator) ([]model.Operator, error)
+	AddTourPackage(userID primitive.ObjectID, tour model.Tour) (bool, error)
+
+	//InsertTour(tour model.Tour) (primitive.ObjectID, error)
+	//DeleteTour(tourID string) (bool, error)
+	//UpdateTour(tourID string, tour model.Tour) (bool, error)
+	//GetTour(tourID string) (tour model.Tour, err error)
+	//FindAllTours() (tours []model.Tour, err error)
 }
--- query/repo/db_query.go
+++ query/repo/db_query.go
@@ -2,7 +2,7 @@ package repo
 
 import (
 	"context"
-	"log"
+	"fmt"
 	"time"
 
 	"github.com/travas-io/travas/model"
@@ -13,140 +13,18 @@ import (
 
 // database queries is done in this file
 
-func (td *TravasDB) InsertTour(tour model.Tour) (primitive.ObjectID, error) {
+func (td *TravasDB) InsertUser(user model.Tourist) (int, primitive.ObjectID, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
 	defer cancel()
-
-	var id primitive.ObjectID
-
-	filter := bson.D{{Key: "tour_title", Value: tour.TourTitle}}
-	var res bson.M
-	err := TouristData(td.DB, "tours").FindOne(ctx, filter).Decode(&res)
-	if err != nil {
-		if err == mongo.ErrNoDocuments {
-			tour.ID = primitive.NewObjectID()
-			doc := bson.D{
-				{Key: "_id", Value: tour.ID},
-				{Key: "operator_id", Value: tour.OperatorID},
-				{Key: "tour_title", Value: tour.TourTitle},
-				{Key: "meeeting_point", Value: tour.MeetingPoint},
-				{Key: "start_time", Value: tour.StartTime},
-				{Key: "language_offered", Value: tour.LanguageOffered},
-				{Key: "number_of_tourist", Value: tour.NumberOfTourist},
-				{Key: "description", Value: tour.Description},
-				{Key: "tour_guide", Value: tour.TourGuide},
-				{Key: "tour_operator", Value: tour.TourOperator},
-				{Key: "operator_contact", Value: tour.OperatorContact},
-				{Key: "date", Value: tour.Date},
-			}
-			_, insertErr := TouristData(td.DB, "tours").InsertOne(ctx, doc)
-			if insertErr != nil {
-				td.App.ErrorLogger.Fatalf("cannot add user to the database : %v ", insertErr)
-			}
-			return tour.ID, nil
-		}
-		td.App.ErrorLogger.Fatal(err)
-	}
-
-	for k, v := range res {
-		if k == "_id" {
-			switch tourID := v.(type) {
-			case primitive.ObjectID:
-				id = tourID
-			}
-		}
-	}
-	return id, err
-}
-
-func (td *TravasDB) DeleteTour(tourID string) (bool, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
-	defer cancel()
-
-	var result bson.M
-
-	filter := bson.D{{Key: "_id", Value: tourID}}
-	err := TouristData(td.DB, "tours").FindOneAndDelete(ctx, filter).Decode(&result)
-	if err != nil {
-		if err == mongo.ErrNoDocuments {
-			td.App.ErrorLogger.Println("no document found for this query")
-			return false, err
-		}
-		td.App.ErrorLogger.Fatalf("cannot execute the database query perfectly : %v ", err)
-	}
-	// td.App.InfoLogger.Printf("found document %v", result)
-	return true, nil
-}
-
-func (td *TravasDB) UpdateTour(tourID string, tour model.Tour) (bool, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
-	defer cancel()
-
-	filter := bson.D{{Key: "_id", Value: tourID}}
-	_, err := TouristData(td.DB, "tours").UpdateByID(ctx, filter, tour)
-	if err != nil {
-		return false, err
-	}
-	return true, nil
-}
-func (td *TravasDB) GetTour(tourID string) (tour model.Tour, err error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
-	defer cancel()
-	filter := bson.D{{Key: "_id", Value: tourID}}
-	err = TouristData(td.DB, "tours").FindOne(ctx, filter).Decode(&tour)
-	if err != nil {
-		if err == mongo.ErrNoDocuments {
-			td.App.ErrorLogger.Println("no document found for this query")
-		}
-		td.App.ErrorLogger.Fatalf("cannot execute the database query perfectly : %v ", err)
-	}
-	return tour, err
-}
-func (td *TravasDB) FindAllTours() (tours []model.Tour, err error) {
-	//	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
-	//	defer cancel()
-	cur, err := TouristData(td.DB, "tours").Find(context.Background(), bson.D{})
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer cur.Close(context.Background())
-
-	if err = cur.All(context.Background(), &tours); err != nil {
-		log.Fatal(err)
-	}
-
-	return tours, err
-
-}
-func (td *TravasDB) InsertUser(user model.Tourist, tours []model.Tour) (int, primitive.ObjectID, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
-	defer cancel()
-
-	var id primitive.ObjectID
 
 	filter := bson.D{{Key: "email", Value: user.Email}}
 
-	var res bson.M
+	var res interface{}
 	err := TouristData(td.DB, "tourist").FindOne(ctx, filter).Decode(&res)
 	if err != nil {
 		if err == mongo.ErrNoDocuments {
 			user.ID = primitive.NewObjectID()
-			doc := bson.D{
-				{Key: "_id", Value: user.ID},
-				{Key: "email", Value: user.Email},
-				{Key: "first_name", Value: user.FirstName},
-				{Key: "last_name", Value: user.LastName},
-				{Key: "phone", Value: user.Phone},
-				{Key: "password", Value: user.Password},
-				{Key: "check_password", Value: user.CheckPassword},
-				{Key: "tours_list", Value: tours},
-				{Key: "created_at", Value: user.CreatedAt},
-				{Key: "updated_at", Value: user.UpdatedAt},
-				{Key: "geo_location", Value: ""},
-				{Key: "token", Value: ""},
-				{Key: "new_token", Value: ""},
-			}
-			_, insertErr := TouristData(td.DB, "tourist").InsertOne(ctx, doc)
+			_, insertErr := TouristData(td.DB, "tourist").InsertOne(ctx, user)
 			if insertErr != nil {
 				td.App.ErrorLogger.Fatalf("cannot add user to the database : %v ", insertErr)
 			}
@@ -155,14 +33,8 @@ func (td *TravasDB) InsertUser(user model.Tourist, tours []model.Tour) (int, pri
 		td.App.ErrorLogger.Fatal(err)
 	}
 
-	for k, v := range res {
-		if k == "_id" {
-			switch userID := v.(type) {
-			case primitive.ObjectID:
-				id = userID
-			}
-		}
-	}
+	id := res.(primitive.ObjectID)
+
 	return 1, id, err
 }
 
@@ -189,7 +61,6 @@ func (td *TravasDB) UpdateInfo(userID primitive.ObjectID, tk map[string]string) 
 	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
 	defer cancel()
 
-	// opt := options.Update().SetUpsert(true)
 	filter := bson.D{{Key: "_id", Value: userID}}
 	update := bson.D{{Key: "$set", Value: bson.D{{Key: "token", Value: tk["t1"]}, {Key: "new_token", Value: tk["t2"]}}}}
 
@@ -198,4 +69,33 @@ func (td *TravasDB) UpdateInfo(userID primitive.ObjectID, tk map[string]string) 
 		return false, err
 	}
 	return true, nil
+}
+
+func (td *TravasDB) LoadPackage(res []model.Operator) ([]model.Operator, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+
+	cursor, err := OperatorData(td.DB, "operator").Find(ctx, bson.D{{}})
+	if err != nil {
+		return res, fmt.Errorf("cannot find document in the database %v ", err)
+	}
+
+	if err = cursor.All(ctx, &res); err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+func (td *TravasDB) AddTourPackage(userID primitive.ObjectID, tour model.Tour) (bool, error){
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+
+	filter := bson.D{{Key: "_id", Value: userID}}
+	update := bson.D{{Key: "$push", Value: bson.D{{Key: "tour_list", Value: tour}}}}
+
+	_, err:= TouristData(td.DB, "tourist").UpdateOne(ctx, filter, update)
+	if err != nil{
+		return false, fmt.Errorf("cannot update document : %v ", err)
+	}
+	return  true, nil
 }
